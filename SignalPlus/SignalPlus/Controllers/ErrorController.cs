47bd0546c2aa144bd215829b8cd5d8118e856301
Tencho <tencho1011@gmail.com>
2025-03-31 09:19:10 +0300

    Create NotFound page

---
+++ SignalPlus/SignalPlus/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿namespace SignalPlus.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("Error/404")]
+        public IActionResult NotFoundPage()
+        {
+            return View("NotFound");
+        }
+
+        [Route("Error/{code}")]
+        public IActionResult Error(int code)
+        {
+            // For any other error, redirect to home
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
