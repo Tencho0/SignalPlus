bf11b155e1fcb78a7dacd61c9f02bc1bec883df6
Tencho <tencho1011@gmail.com>
2025-05-03 19:24:31 +0300

    Added main Section

--- SignalPlus/SignalPlus/Controllers/HomeController.cs
+++ SignalPlus/SignalPlus/Controllers/HomeController.cs
@@ -1,21 +1,35 @@
 namespace SignalPlus.Controllers
 {
     using System.Diagnostics;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using SignalPlus.DTOs;
     using SignalPlus.Models;
+    using SignalPlus.Services.Interfaces;
 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ISignalService _signalService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ISignalService signalService)
         {
             _logger = logger;
+            _signalService = signalService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            var total = await _signalService.GetTotalSignalsCountAsync();
+            var completed = await _signalService.GetCompletedSignalsCountAsync();
+            
+            var homePageDTO = new HomePageDTO
+            {
+                TotalSignals = total,
+                CompletedSignals = completed
+            };
+
+            return View(homePageDTO);
         }
 
         public IActionResult Privacy()
---
+++ SignalPlus/SignalPlus/DTOs/HomePageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalPlus.DTOs
+{
+    public class HomePageDTO
+    {
+        public int TotalSignals { get; set; }
+
+        public int CompletedSignals { get; set; }
+    }
+}
